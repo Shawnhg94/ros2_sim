2f707119a823e69fd341b37a9d85e19a0a635fe8
shawn <shawnhg28@gmail.com>
2025-05-22 18:17:06 +0800

    update controlnode

--- src/control_package/control_package/control_node.py
+++ src/control_package/control_package/control_node.py
@@ -6,15 +6,14 @@ from enum import Enum
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
-from nav_msgs.msg import Odometry
 from sensor_msgs.msg import Joy, LaserScan, NavSatFix
 from std_msgs.msg import Float32, Int32, Float32MultiArray
-from tf_transformations import euler_from_quaternion
+
 
 class ControlNode(Node):
     def __init__(self):
+        print('Create ControlNode')
         super().__init__('control_node')
-        self.mutex = threading.Lock()
 
         # Publishers
         self.cmd_pub = self.create_publisher(Twist, 'cmd_vel', 10)
@@ -22,6 +21,54 @@ class ControlNode(Node):
         # Subscribers
         self.create_subscription(Joy, 'joy', self.joy_cb, 10)
         self.create_subscription(LaserScan, 'scan', self.lidar_cb, 10)
+        
+        # Timer
+        self.create_timer(0.1, self.timer_cb)
+        
+        # Variables
+        self.is_obstacle = False
+        self.minRange = 0.0
+        self.minAngle = 0.0
+        self.turning_counter = 0
+        
+        self.angle_counter = -1
+        self.step_counter = 0
+        self.is_turning = False
+        self.auto_mode = True
+        self.publish_twist(0.7, 0.0)
+        
+    def joy_cb(self, msg: Joy):
+        if (self.auto_mode):
+            return
+        if msg.buttons[0]:  # X (PS4)
+            self.auto_mode = True
+            self.publish_twist(0.7, 0.0)
+    
+    def lidar_cb(self, msg: LaserScan):
+        if (self.is_turning):
+            return
+        
+        dist = []
+        angle = -30.0
+        
+        for i in range(len(msg.ranges)):
+            angle = angle + i
+            dist.append(self.getRangeAtDegree(msg, angle))
+            
+        self.minRange, self.minAngle = self.getMinRange(dist)
+        if (self.minRange < 0.7):
+            self.is_obstacle = True
+        else:
+            self.is_obstacle = False
+            
+        if (self.is_obstacle):
+            self.publish_twist(0.0, 0.0)
+            if (self.step_counter % 2 == 0):
+                self.turn_angle(-90.0)
+            else:
+                self.turn_angle(90.0)
+            self.step_counter += 1
+            
         
         
     def getRangeAtDegree(self, scan: LaserScan, angle_degrees: float):
@@ -32,11 +79,11 @@ class ControlNode(Node):
         center_index = int(- scan.angle_min / scan.angle_increment)
         index = center_index + int(angle_radians / scan.angle_increment)
         
-        if (index >= 0 and index < len(scan.range)):
+        if (index >= 0 and index < len(scan.ranges)):
             return scan.ranges[index]
         return -1.0
     
-    def getMinRange(angle_list: list):
+    def getMinRange(self, angle_list: list):
         min_range = math.inf
         min_index = 0
         
@@ -47,11 +94,46 @@ class ControlNode(Node):
         
         angle = -30.0 + float(min_index)
         return min_range, angle
+    
+    def timer_cb(self):
+        if self.angle_counter < 0:
+            return
+        
+        if self.angle_counter >= self.target_counter:
+            self.angle_counter = -1
+            self.is_turning = False
+            self.publish_twist(0.7, 0.0)
+        
+        if (self.step_counter > 10):
+            self.auto_mode = False
+            self.publish_twist(0.0, 0.0)
+            
+        self.angle_counter += 1
+        
+            
+    def turn_angle(self, angle):
+        angular_sp = 0.314
+        if angle < 0.0:
+            angular_sp = -0.314
+        else:
+            angular_sp = 0.314
+            
+        self.target_counter = int(abs(angle) / 1.8)
+            
+        self.is_turning = True
+        
+        self.publish_twist(0.0, angular_sp)
+        
+    def publish_twist(self, lin: float, ang: float):
+        t = Twist()
+        t.linear.x = lin
+        t.angular.z = ang
+        self.cmd_pub.publish(t)
+        
 
 def main():
-    print('Hi from control_package.')
+    rclpy.init()
+    rclpy.spin(ControlNode())
+    rclpy.shutdown()
 
-
-if __name__ == '__main__':
-    main()
 
